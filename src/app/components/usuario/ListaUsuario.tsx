c72ba451d4c188bc4517ddd84b5bb2cea9e3535b
Limber Jhonathan <limberjhonathan@gmail.com>
2025-03-05 23:42:58 -0300

    ajuste da lista de usuario

--- src/app/components/usuario/ListaUsuario.tsx
+++ src/app/components/usuario/ListaUsuario.tsx
@@ -1,10 +1,15 @@
 import usuarios from "@/app/data/constants/usuario";
 import LinhaUsuario from "./LinhaUsuario";
+import { Usuario } from "@/core/model/Usuario";
 
-export default function ListaUsuario() {
+export interface ListaUsuarioProps {
+    onClick?: (usuario: Usuario) => void
+}
+
+export default function ListaUsuario(props: ListaUsuarioProps) {
     return <div className="flex flex-col gap-4">
         {usuarios.map((usuario , i) => {
-            return <LinhaUsuario key={i} usuario={usuario}/>
+            return <LinhaUsuario key={i} usuario={usuario} onClick={props.onClick}/>
         })}
     </div>
 }
