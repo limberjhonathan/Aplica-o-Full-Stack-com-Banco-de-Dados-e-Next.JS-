0a0503b4a25f3d7a2efe1c54eff687a7605723fc
Limber Jhonathan <limberjhonathan@gmail.com>
2025-03-06 23:45:09 -0300

    ajuste do props

--- src/app/components/usuario/ListaUsuario.tsx
+++ src/app/components/usuario/ListaUsuario.tsx
@@ -1,14 +1,14 @@
-import usuarios from "@/app/data/constants/usuario";
 import LinhaUsuario from "./LinhaUsuario";
-import { Usuario } from "@/core/model/Usuario";
+import { Usuario } from "@prisma/client";
 
 export interface ListaUsuarioProps {
+    usuarios: Usuario[]
     onClick?: (usuario: Usuario) => void
 }
 
 export default function ListaUsuario(props: ListaUsuarioProps) {
     return <div className="flex flex-col gap-4">
-        {usuarios.map((usuario , i) => {
+        {props.usuarios.map((usuario , i) => {
             return <LinhaUsuario key={i} usuario={usuario} onClick={props.onClick}/>
         })}
     </div>
