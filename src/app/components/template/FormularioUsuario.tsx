6b8f97c9e68dc57f47b121633a4dfa9f2d2d172f
Limber Jhonathan <limberjhonathan@gmail.com>
2025-03-07 22:26:08 -0300

    criaçào das btns

--- src/app/components/template/FormularioUsuario.tsx
+++ src/app/components/template/FormularioUsuario.tsx
@@ -2,10 +2,11 @@ import { Usuario } from "@/core/model/Usuario"
 import InputTexto from "../shared/inputTexto"
 
 export interface FormularioUsuarioProps {
-    usuario: Usuario
-    onChange: (usuario: Usuario) => void
+    usuario: Partial<Usuario>
+    onChange: (usuario: Partial<Usuario>) => void
     salvar: () => void
     cancelar: () => void
+    excluir: () => void
 }
 
 export default function FormularioUsuario(props: FormularioUsuarioProps) {
@@ -13,18 +14,21 @@ export default function FormularioUsuario(props: FormularioUsuarioProps) {
 
     return (
         <div>
-            <InputTexto label="Nome" type="text" value={props.usuario.nome} onChange={
+            <InputTexto label="Nome" type="text" value={props.usuario.nome ?? ""} onChange={
                 e => props.onChange?.({ ...props.usuario, nome: e.target.value })
-            }/>
-            <InputTexto label="E-mail" type="email" value={props.usuario.email} onChange={
+            } />
+            <InputTexto label="E-mail" type="email" value={props.usuario.email ?? ""} onChange={
                 e => props.onChange?.({ ...props.usuario, email: e.target.value })
-            }/>
-            <InputTexto label="Senha" type="password" value={props.usuario.senha} onChange={
+            } />
+            <InputTexto label="Senha" type="password" value={props.usuario.senha ?? ""} onChange={
                 e => props.onChange?.({ ...props.usuario, senha: e.target.value })
-            }/>
-            <div className="flex gap-5">
-                <button className="bg-blue-500 px-4 py-2 rounded-md" onClick={props.salvar}>Salvar</button>
-                <button className="bg-zinc-500 px-4 py-2 rounded-md" onClick={props.cancelar}>Cancelar</button>
+            } />
+            <div className="flex justify-between">
+                <div className="flex gap-5">
+                    <button className="bg-blue-500 px-4 py-2 rounded-md" onClick={props.salvar}>Salvar</button>
+                    <button className="bg-zinc-500 px-4 py-2 rounded-md" onClick={props.cancelar}>Cancelar</button>
+                </div>
+                <button className="bg-red-500 px-4 py-2 rounded-md" onClick={props.excluir}>excluir</button>
             </div>
         </div>
     );
