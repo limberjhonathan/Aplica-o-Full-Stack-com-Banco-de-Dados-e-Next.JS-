bb10dd58ad0d5413e4c23772b5090703295b0443
Limber Jhonathan <limberjhonathan@gmail.com>
2025-03-07 22:26:59 -0300

    principais ajustes

--- src/app/(internas)/usuarios/page.tsx
+++ src/app/(internas)/usuarios/page.tsx
@@ -3,24 +3,33 @@
 import FormularioUsuario from "@/app/components/template/FormularioUsuario";
 import Pagina from "@/app/components/template/Pagina";
 import Titulo from "@/app/components/template/Titulo";
+import ListaUsuario from "@/app/components/usuario/ListaUsuario";
+import useUsuarios from "@/app/data/hooks/useUsuarios";
 // import ListaUsuario from "@/app/components/usuario/ListaUsuario";
-import usuarios from "@/app/data/constants/usuario";
-import { Usuario } from "@prisma/client";
-import { IconUser } from "@tabler/icons-react";
-import { useState } from "react";
+import { IconPlus, IconUser } from "@tabler/icons-react";
 
-export default function Page(){
-    const [usuario, setUsuario] = useState<Usuario>(usuarios[0])
 
-    function salvar() {
-        //Salvar no banco de dados
-    }
+export default function Page() {
+    const { usuarios, usuario, salvar, excluir, alterarUsuario } = useUsuarios()
 
     return (
         <Pagina className="flex flex-col gap-10">
-            <Titulo icone={IconUser} principal="Usuários" segundario="Cadastro de usuário"/>
+            <Titulo icone={IconUser} principal="Usuários" segundario="Cadastro de usuário" />
+
             {/* <ListaUsuario/> */}
-            <FormularioUsuario usuario={usuario} onChange={setUsuario} cancelar={() => {}} salvar={salvar}/>
+            {usuario ? (
+                <FormularioUsuario usuario={usuario} onChange={alterarUsuario} cancelar={() => alterarUsuario(null)} salvar={salvar} excluir={excluir}/>
+            ) : (
+                <>
+                    <div className="flex justify-end">
+                        <button className="flex items-center gap-2 bg-blue-500 px-4 py-2 rounded-md" onClick={() => alterarUsuario({})}>
+                            <IconPlus />
+                            <span>Novo Usuário</span>
+                        </button>
+                    </div>
+                    <ListaUsuario usuarios={usuarios} onClick={alterarUsuario} />
+                </>
+            )}
         </Pagina>
     )
 }
